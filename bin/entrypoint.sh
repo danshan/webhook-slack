dcbff1a42933a9626d0ca7c6f1a29086ecf6ecf0
dan <i@shanhh.com>
2017-03-22 10:03:17 +0800

    feature update readme

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -4,4 +4,5 @@ java -jar \
   -Dslack.daocloud="$SLACK_DAOCLOUD" \
   -Dslack.microbadger="$SLACK_MICROBADGER" \
   -Dslack.docker="$SLACK_DOCKER" \
+  -Dslack.sonar="$SLACK_SONAR" \
   $DATA_DIR/$APP_NAME/target/$APP_NAME.jar
