80852fb343a85da0d3efa53fbec8ea59b65e7719
dan <i@shanhh.com>
2017-06-02 08:21:43 +0800

    feature update entrypoint for coding

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -5,4 +5,5 @@ java -jar \
   -Dslack.microbadger="$SLACK_MICROBADGER" \
   -Dslack.docker="$SLACK_DOCKER" \
   -Dslack.sonar="$SLACK_SONAR" \
+  -Dslack.coding="$SLACK_CODING" \
   $DATA_DIR/$APP_NAME/target/$APP_NAME.jar
